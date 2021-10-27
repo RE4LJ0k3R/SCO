1301cee1d89040c3359309249f4bd5554f44bfa7
r3alj0k3r <karlheinzpeter011@gmail.com>
2021-10-27 17:09:47 +0200

    added save for the database after closing the program with keyboard interrupt

--- main.py
+++ main.py
@@ -86,99 +86,105 @@ def main(sc_name, ribs_enabled, path):
     stream = get_latest_rib_stream(sc_name)
 
     logging.info("Start building update database")
-    for rec in stream:
-        for elem in rec:
-            prefix = elem.fields['prefix']
+    try:
+        for rec in stream:
+            for elem in rec:
+                prefix = elem.fields['prefix']
 
-            if tree_v4.get(prefix):  # filter out prefixes that belong to the as company as well as longer ones
-                continue
+                if tree_v4.get(prefix):  # filter out prefixes that belong to the as company as well as longer ones
+                    logging.info("Prefix is company prefix: " + prefix)
+                    continue
 
-            if ribs_enabled:
-                if elem.type == 'R':
-                    if not (prefix in updates):
-                        updates[prefix] = {'announcement': {'counter': 1}, 'withdrawal': {'counter': 0}, 'start_time': None,
-                                           'stop_thread_queue': None}
-                        if not (elem.peer_asn in updates[prefix]['announcement']):
-                            updates[prefix]['announcement'][elem.peer_asn] = elem.time
-
-                else:
-                    if check_time(rec.time):
-                        logging.info("Now we are in real time")
-                        ribs_enabled = False
-
-                    if elem.type == "A":
+                if ribs_enabled:
+                    if elem.type == 'R':
                         if not (prefix in updates):
                             updates[prefix] = {'announcement': {'counter': 1}, 'withdrawal': {'counter': 0}, 'start_time': None,
                                                'stop_thread_queue': None}
-                        else:
-                            updates[prefix]['announcement']['counter'] += 1
-                        if not (elem.peer_asn in updates[prefix]['announcement']):
-                            updates[prefix]['announcement'][elem.peer_asn] = elem.time
-
-                    elif elem.type == "W":
-                        if prefix in updates:
                             if not (elem.peer_asn in updates[prefix]['announcement']):
-                                updates[prefix]['withdrawal'][elem.peer_asn] = elem.time
-                            if updates[prefix]['withdrawal']['counter'] > UPDATE_THRESHOLD:
-                                save_as_updates(prefix, elem.peer_asn, updates[prefix])
-                                del updates[prefix]
-
-            else:
-                if elem.type == 'A':
-                    if not (prefix in updates):
-                        stop_queue = Queue()
-                        updates[prefix] = {'announcement': {'counter': 1}, 'withdrawal': {'counter': 0}, 'start_time': elem.time,
-                                           'stop_thread_queue': stop_queue}
+                                updates[prefix]['announcement'][elem.peer_asn] = elem.time
 
                     else:
-                        updates[prefix]['announcement']['counter'] += 1
+                        if check_time(rec.time):
+                            logging.info("Now we are in real time")
+                            ribs_enabled = False
 
-                    if not (elem.peer_asn in updates[prefix]['announcement']):
-                        updates[prefix]['announcement'][elem.peer_asn] = elem.time
-                    if updates[prefix]['announcement']['counter'] == UPDATE_THRESHOLD:
-                        logging.info("Threshold reached for prefix " + prefix)
-                        # limitation part
-                        files = glob.glob('*')
-                        files = filter(lambda x: not x.startswith(as_num), files)
-                        if len(files) > 11:  # check if other as had a protection event
-                            continue
+                        if elem.type == "A":
+                            if not (prefix in updates):
+                                updates[prefix] = {'announcement': {'counter': 1}, 'withdrawal': {'counter': 0}, 'start_time': None,
+                                                   'stop_thread_queue': None}
+                            else:
+                                updates[prefix]['announcement']['counter'] += 1
+                            if not (elem.peer_asn in updates[prefix]['announcement']):
+                                updates[prefix]['announcement'][elem.peer_asn] = elem.time
 
-                        stop_queue = updates[prefix]['stop_thread_queue']
-                        if stop_queue is None:  # addition in order to only start measurements for recent prefixes
-                            logging.info("Stop queue is None")
-                            logging.info("Start time is " + str(updates[prefix]['start_time']))
-                            continue
+                        elif elem.type == "W":
+                            if prefix in updates:
+                                if not (elem.peer_asn in updates[prefix]['announcement']):
+                                    updates[prefix]['withdrawal'][elem.peer_asn] = elem.time
+                                if updates[prefix]['withdrawal']['counter'] > UPDATE_THRESHOLD:
+                                    save_as_updates(prefix, elem.peer_asn, updates[prefix])
+                                    del updates[prefix]
 
-                        if measurement_counter >= MEASUREMENT_THRESHOLD:
-                            continue
+                else:
+                    if elem.type == 'A':
+                        if not (prefix in updates):
+                            logging.info("Found new prefix: " + prefix)
+                            stop_queue = Queue()
+                            updates[prefix] = {'announcement': {'counter': 1}, 'withdrawal': {'counter': 0}, 'start_time': elem.time,
+                                               'stop_thread_queue': stop_queue}
+
                         else:
-                            measurement_counter += 1
+                            updates[prefix]['announcement']['counter'] += 1
 
-                        Thread(target=call_measurement,
-                               args=(prefix, sc_name, elem.time, stop_queue)).start()
-                        logging.info("Started thread for prefix " + prefix)
-
-                elif elem.type == 'W':
-                    if prefix in updates:
-                        updates[prefix]['withdrawal']['counter'] += 1
-                        if updates[prefix]['withdrawal']['counter'] == 1:
-                            updates[prefix]['end_time'] = elem.time
                         if not (elem.peer_asn in updates[prefix]['announcement']):
-                            updates[prefix]['withdrawal'][elem.peer_asn] = elem.time
+                            updates[prefix]['announcement'][elem.peer_asn] = elem.time
+                        if updates[prefix]['announcement']['counter'] == UPDATE_THRESHOLD:
+                            logging.info("Threshold reached for prefix " + prefix)
+                            # limitation part
+                            files = glob.glob('*')
+                            files = filter(lambda x: not x.startswith(as_num), files)
+                            if len(files) > 11:  # check if other as had a protection event
+                                continue
 
-                        if updates[prefix]['withdrawal']['counter'] == UPDATE_THRESHOLD:
                             stop_queue = updates[prefix]['stop_thread_queue']
-                            if stop_queue and stop_queue.empty():
-                                stop_queue.put(True)
+                            if stop_queue is None:  # addition in order to only start measurements for recent prefixes
+                                logging.info("Stop queue is None")
+                                logging.info("Start time is " + str(updates[prefix]['start_time']))
+                                continue
 
-                                Thread(target=call_measurement,
-                                       args=(prefix, sc_name, elem.time, None)).start()
+                            if measurement_counter >= MEASUREMENT_THRESHOLD:
+                                continue
+                            else:
+                                measurement_counter += 1
 
-                        if updates[prefix]['withdrawal']['counter'] > UPDATE_THRESHOLD * 2:
-                            if updates[prefix]['stop_thread_queue']:
-                                measurement_counter -= 1
-                            logging.info("Deleting entry for prefix " + prefix)
-                            del updates[prefix]  # just some cleanup since all measurements should be started
+                            Thread(target=call_measurement,
+                                   args=(prefix, sc_name, elem.time, stop_queue)).start()
+                            logging.info("Started thread for prefix " + prefix)
+
+                    elif elem.type == 'W':
+                        if prefix in updates:
+                            updates[prefix]['withdrawal']['counter'] += 1
+                            if updates[prefix]['withdrawal']['counter'] == 1:
+                                updates[prefix]['end_time'] = elem.time
+                            if not (elem.peer_asn in updates[prefix]['announcement']):
+                                updates[prefix]['withdrawal'][elem.peer_asn] = elem.time
+
+                            if updates[prefix]['withdrawal']['counter'] == UPDATE_THRESHOLD:
+                                stop_queue = updates[prefix]['stop_thread_queue']
+                                if stop_queue and stop_queue.empty():
+                                    stop_queue.put(True)
+
+                                    Thread(target=call_measurement,
+                                           args=(prefix, sc_name, elem.time, None)).start()
+
+                            if updates[prefix]['withdrawal']['counter'] > UPDATE_THRESHOLD * 2:
+                                if updates[prefix]['stop_thread_queue']:
+                                    measurement_counter -= 1
+                                logging.info("Deleting entry for prefix " + prefix)
+                                del updates[prefix]  # just some cleanup since all measurements should be started
+    except KeyboardInterrupt:
+        with open("database_" + as_num, 'w+') as fd:
+            json.dump(updates, fd)
 
 
 if __name__ == '__main__':
@@ -217,5 +223,5 @@ if __name__ == '__main__':
         print(str(err))
         exit(1)
 
-    except KeyboardInterrupt:
-        sys.exit(0)
+    #except KeyboardInterrupt:
+    #    sys.exit(0)
